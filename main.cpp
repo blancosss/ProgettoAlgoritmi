9a3bc4517d358e321b97d2cba87cca8a3a56fccc
Samz-hash <83223400+samz-hash@users.noreply.github.com>
2021-04-26 14:51:10 +0200

    Prova

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Paolo Bianchi on 26/04/21.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Paolo Bianchi on 26/04/21.
+//
+
+#ifndef PROGETTOALGORITMI_MAIN_H
+#define PROGETTOALGORITMI_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PROGETTOALGORITMI_MAIN_H
